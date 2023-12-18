14cbf97e59f3df0ab0f0bb2cc8f4400bb39f39dd
Dan Shernicoff <dshernicoff@tenable.com>
2023-12-18 10:55:42 -0500

    PyCharm metadata.

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,29 @@
+from aoc_lib import solve_problem
+
+INPUT = ''''''
+
+TEST_INPUT = ''''''
+
+
+def solve(input_: str) -> int:
+    pass
+    
+
+if __name__ == '__main__':
+    part1_args = []
+    expected_1 = []
+    func_1 = solve
+
+    part2_args = []
+    expected_2 = []
+    func_2 = solve
+
+    if expected_1:
+        for idx, (e_total, e_params) in enumerate(expected_1):
+            solve_problem(func_1, 1, (idx, e_total), *e_params)
+        solve_problem(func_1, 1, None, INPUT, *part1_args)
+
+    if expected_2:
+        for idx, (e_total, e_params) in enumerate(expected_2):
+            solve_problem(func_2, 2, (idx, e_total), *e_params)
+        solve_problem(func_2, 2, None, INPUT, *part2_args)
