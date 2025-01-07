8e09181cc6acf3a0664c4d4f5cecb4261e5f2bd2
Dan Shernicoff <dshernicoff@tenable.com>
2025-01-07 10:19:16 -0500

    Day 22

--- aoc_2024/day22.py
+++ aoc_2024/day22.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from functools import cache
 from multiprocessing.pool import Pool
 
@@ -10,8 +11,14 @@ TEST_INPUT = """1
 100
 2024"""
 
+TEST_INPUT2 = """1
+2
+3
+2024"""
+
+
 @cache
-def mix(num1: int, num2 : int) -> int:
+def mix(num1: int, num2: int) -> int:
     return num1 ^ num2
 
 
@@ -21,17 +28,54 @@ def prune(num: int) -> int:
 
 
 def get_secret(secret: int) -> int:
-    for _ in range(2000):
-        secret = prune(mix(secret, secret * 64))
-        secret = prune(mix(secret, secret // 32))
-        secret = prune(mix(secret, secret * 2048))
+    secret = prune(mix(secret, secret * 64))
+    secret = prune(mix(secret, secret // 32))
+    secret = prune(mix(secret, secret * 2048))
     return secret
 
 
+def get_last_secret(secret: int) -> int:
+    for _ in range(2000):
+        secret = get_secret(secret)
+    return secret
+
+
+def build_price_db(secret: int) -> tuple[list[int], list[int]]:
+    prices, deltas = list(), list()
+    curr_secret = secret
+    last_price = secret % 10
+    for _ in range(2000):
+        new_secret = get_secret(curr_secret)
+        price = new_secret % 10
+        prices.append(price)
+        deltas.append(price - last_price)
+        last_price = price
+        curr_secret = new_secret
+    return deltas, prices
+
+
+def get_combo_prices(secret: int) -> dict:
+    combo_prices = dict()
+    deltas, prices = build_price_db(secret)
+    for i in range(len(deltas) - 4):
+        combo = tuple(deltas[i : i + 4])
+        if combo in combo_prices:
+            continue
+        combo_prices[combo] = prices[i + 3]
+    return combo_prices
+
+
 def solve(input_: str) -> int:
-    secrets = [int(line) for line in input_.splitlines()]
-    extrapolated = list(Pool(12).imap(get_secret, secrets))
-    return sum(extrapolated)
+    return sum(Pool(12).map(get_last_secret, (int(line) for line in input_.splitlines())))
+
+
+def solve2(input_: str) -> int:
+    combo_prices = defaultdict(int)
+    for combo_price in Pool(12).map(get_combo_prices, (int(line) for line in input_.splitlines())):
+        for combo, price in combo_price.items():
+            combo_prices[combo] += price
+
+    return max(combo_prices.values())
 
 
 if __name__ == '__main__':
@@ -40,8 +84,8 @@ if __name__ == '__main__':
     func_1 = solve
 
     part2_args = []
-    expected_2 = []
-    func_2 = solve
+    expected_2 = [(23, [TEST_INPUT2])]
+    func_2 = solve2
 
     if expected_1:
         for idx, (e_total, e_params) in enumerate(expected_1):
