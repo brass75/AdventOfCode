76a7e6a67c6313b238781e9e3fc3e68fb07791ba
Dan Shernicoff <dshernicoff@tenable.com>
2025-01-09 09:24:01 -0500

    Day 25

---
+++ aoc_2024/day25.py
@@ -0,0 +1,92 @@
+from aoc_lib import solve_problem
+
+INPUT = open('data/day25.txt').read()
+
+TEST_INPUT = """#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"""
+
+
+def parse_lock(lock: str) -> list[int]:
+    lock_lines = lock.splitlines()
+    cols = [0] * len(lock_lines[0])
+    for line in lock_lines[1:]:
+        for j,c  in enumerate(line):
+            cols[j] += c == '#'
+    return cols
+
+
+def parse_input(input_: str) -> tuple[list, list, int]:
+    keys, locks = list(), list()
+    key_locks = input_.split('\n\n')
+    for key_lock in key_locks:
+        if key_lock[0] == '#':
+            locks.append(parse_lock(key_lock))
+        else:
+            keys.append(parse_lock(key_lock))
+    return locks, keys, len(key_locks[0].splitlines())
+
+
+def solve(input_: str) -> int:
+    locks, keys, height = parse_input(input_)
+    count = 0
+    for lock in locks:
+        for key in keys:
+            count += all(kc + lc < height for lc,kc in zip(lock, key))
+    return count
+
+
+if __name__ == '__main__':
+    part1_args = []
+    expected_1 = [(3, [TEST_INPUT])]
+    func_1 = solve
+
+    part2_args = []
+    expected_2 = []
+    func_2 = solve
+
+    if expected_1:
+        for idx, (e_total, e_params) in enumerate(expected_1):
+            solve_problem(func_1, 1, (idx + 1, e_total), *e_params)
+        solve_problem(func_1, 1, None, INPUT, *part1_args)
+
+    if expected_2:
+        for idx, (e_total, e_params) in enumerate(expected_2):
+            solve_problem(func_2, 2, (idx + 1, e_total), *e_params)
+        solve_problem(func_2, 2, None, INPUT, *part2_args)
