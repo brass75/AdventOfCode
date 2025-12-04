628f2b9615501f3429e5d2c72866a76de3644ab3
Dan Shernicoff <dshernicoff@tenable.com>
2025-12-04 09:36:40 -0500

    Day 4

---
+++ aoc_2025/day4.py
@@ -0,0 +1,64 @@
+from aoc_lib import DIRECTIONS, GridBase, get_adjacent, solve_problem
+
+INPUT = open('data/day4.txt').read()
+
+TEST_INPUT = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
+MAX_ADJACENT = 4
+
+
+def solve(input_: str, remove: bool = False) -> int:
+    grid = GridBase(input_.strip())
+    count = find_movable(grid)
+    while remove and (new_count := find_movable(grid)):
+        count += new_count
+    return count
+
+
+def find_movable(grid: GridBase) -> int:
+    count = 0
+    movable = list()
+    for spot, val in grid.items:
+        if val != '@':
+            continue
+        adj = 0
+        for direction in DIRECTIONS:
+            if (adj := adj + (grid.get(get_adjacent(direction, spot)) == '@')) >= MAX_ADJACENT:
+                break
+        else:
+            count += 1
+            movable.append(spot)
+    for spot in movable:
+        grid.add_obstacle(spot, '.')
+    return count
+
+
+if __name__ == '__main__':
+    part1_args = []
+    expected_1 = [(13, [TEST_INPUT])]
+    func_1 = solve
+
+    part2_args = [True]
+    expected_2 = [(43, [TEST_INPUT, True])]
+    func_2 = solve
+
+    if expected_1:
+        for idx, (e_total, e_params) in enumerate(expected_1):
+            solve_problem(func_1, 1, (idx + 1, e_total), *e_params)
+        solve_problem(func_1, 1, None, INPUT, *part1_args)
+
+    if expected_2:
+        for idx, (e_total, e_params) in enumerate(expected_2):
+            solve_problem(func_2, 2, (idx + 1, e_total), *e_params)
+        solve_problem(func_2, 2, None, INPUT, *part2_args)
