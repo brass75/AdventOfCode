b744a84d61fd52a21cf95fc4a3b39204e0b1dd5f
Dan Shernicoff <dshernicoff@tenable.com>
2025-12-10 11:44:31 -0500

    Day 10, part 2.

--- aoc_2025/day10.py
+++ aoc_2025/day10.py
@@ -1,5 +1,8 @@
+import functools
 import re
 import itertools
+from multiprocessing.pool import ThreadPool as Pool
+from scipy.optimize import linprog
 from aoc_lib import solve_problem
 
 # TODO Point this to the correct day!
@@ -35,9 +38,46 @@ def check_toggles(machine: dict) -> int:
                     return n
     return 0
 
-def solve(input_: str) -> int:
+
+# I'm leaving the brute force solution in here because it _should_ work but with the full data set it's
+# going to take too long to actually run.
+def check_joltages(machine: dict) -> int:
+    @functools.cache
+    def run_combo(combo, num_lights) -> list[int]:
+        lights = [0 for _ in range(num_lights)]
+        all_lights = set()
+        for toggles in combo:
+            all_lights.update(toggles)
+        if not len(all_lights) == len(lights):
+            return lights
+        for toggles in combo:
+            for index in toggles:
+                lights[index] += 1
+        return lights
+    for n in range(1, 1_000_000):
+        if any(lights == machine['jolts']
+               for lights in Pool(8).starmap(run_combo,
+                                             ((combo, len(machine['lights']))
+                                              for combo in itertools.combinations_with_replacement(machine['buttons'], r=n)))
+               ):
+            return n
+    return 0
+
+
+# Since the only way to solve this in a reasonable amount of time is with linear algebra and I don't
+# know linear algebra I'm going to let experts handle it for me.
+def scipy_part2(machine: dict) -> int:
+    return linprog(
+        c=[1] * len(machine['buttons']),
+        A_eq=[[i in move for move in machine['buttons']] for i in range(len(machine['jolts']))],
+        b_eq=machine['jolts'],
+        integrality=True
+    ).fun
+
+
+def solve(input_: str, increment: bool = False) -> int:
     machines = parse_input(input_)
-    return sum(check_toggles(machine) for machine in machines)
+    return sum(check_toggles(machine) for machine in machines) if not increment else sum(map(scipy_part2, machines))
 
 
 
@@ -46,8 +86,8 @@ if __name__ == '__main__':
     expected_1 = [(7, [TEST_INPUT])]
     func_1 = solve
 
-    part2_args = []
-    expected_2 = []
+    part2_args = [True]
+    expected_2 = [(33, [TEST_INPUT, True])]
     func_2 = solve
 
     if expected_1:
