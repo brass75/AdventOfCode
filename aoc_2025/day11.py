e3a830234278abc8145681b7ee2f00d928f4117d
Dan Shernicoff <dshernicoff@tenable.com>
2025-12-11 10:45:19 -0500

    Day 11 part 2

--- aoc_2025/day11.py
+++ aoc_2025/day11.py
@@ -18,33 +18,49 @@ hhh: ccc fff iii
 iii: out
 """
 
+TEST_INPUT2 = """
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+"""
+
+
 def parse_input(input_: str) -> HashableDict:
     node_map = HashableDict()
     for line in input_.strip().splitlines():
-        match = re.match(r'(.*?):\s(.*)', line)
-        node, nodes = match.groups()
-        nodes = frozenset(map(str.strip, nodes.split()))
-        node_map[node] = nodes
+        node, nodes = re.match(r'(.*?):\s(.*)', line).groups()
+        node_map[node] = frozenset(map(str.strip, nodes.split()))
     return node_map
 
 
 @functools.cache
-def find_out(node: str, node_map: HashableDict, nodes: frozenset[str]) -> list[frozenset[str]]:
-    rc = list()
+def find_out(node: str, node_map: HashableDict, filter_for: tuple[str], found: tuple[str]) -> int:
+    if node in filter_for:
+        # If we are filtering (part 2) and we found a node we're looking for, mark it down.
+        found += (node,)
     next_nodes = node_map[node]
-    if 'out' not in next_nodes:
-        for next_node in next_nodes:
-            rc.extend(find_out(next_node, node_map, frozenset([*nodes, node])))
-    else:
-        rc = [frozenset([*nodes, node])]
-    return rc
+    return (
+        # Keep checking the next nodes to see if this is a viable path.
+        sum(find_out(next_node, node_map, filter_for, found) for next_node in next_nodes)
+        if 'out' not in next_nodes
+        # If this is a viable end node (node is "out" and the filter requirements have been met) return 1
+        # The route isn't viable since we haven't met the filter requirements so return 0
+        else not filter_for or found == filter_for
+    )
 
 
-
-def solve(input_: str) -> int:
-    node_map = parse_input(input_)
-    paths = find_out('you', node_map, frozenset())
-    return len(paths)
+def solve(input_: str, start: str = 'you', filter_for: tuple[str] = None) -> int:
+    return find_out(start, parse_input(input_), filter_for or tuple(), tuple())
 
 
 if __name__ == '__main__':
@@ -52,8 +68,8 @@ if __name__ == '__main__':
     expected_1 = [(5, [TEST_INPUT])]
     func_1 = solve
 
-    part2_args = []
-    expected_2 = []
+    part2_args = ['svr', ('fft', 'dac')]
+    expected_2 = [(2, [TEST_INPUT2, *part2_args])]
     func_2 = solve
 
     if expected_1:
