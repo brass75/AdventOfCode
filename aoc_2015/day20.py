fee7c850e51a64e51cdc87f64ff8adfb001e30a1
Dan Shernicoff <dshernicoff@tenable.com>
2026-08-03 15:06:09 -0400

    2015 day 20 part 2

--- aoc_2015/day20.py
+++ aoc_2015/day20.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 from aoc_lib import solve_problem
@@ -10,12 +11,25 @@ def parse_input(input_: str) -> int:
 INPUT = parse_input(Path('data/day20.txt').read_text())
 
 
-def solve(count: int) -> int:
-    houses = [0 for _ in range(count // 10)]
-    for elf in range(1, count // 10):
-        for house in range(elf, count // 10, elf):
-            houses[house] += elf * 10
-    return min(i for i, presents in enumerate(houses) if presents >= count)
+def solve(count: int, presents: int = 10, max_houses: int = 0) -> int:
+    quotas = defaultdict(int)
+    # Set the number of iterations. For the test data it anything larger than 16 will trigger a failure becausee
+    # there won't be enough houses. For the real data 40 work (50 doesn't) and gets the runtime under a second.
+    end = count // 40 if count > 1000 else 16
+
+    # Use a default dict for the houses to accumulate the number of presents. It's a bit slower than using a
+    # prepopulated list but it's more intuitive.
+    houses = defaultdict(int)
+    for elf in range(1, end):
+        # By advancing by the elf we don't need to do any pesky modulos here.
+        for house in range(elf, end, elf):
+            if max_houses:
+                if quotas[elf] > max_houses:
+                    # Once an elf has delivered to all its houses it's done.
+                    continue
+                quotas[elf] += 1
+            houses[house] += elf * presents
+    return min(house for house, presents in houses.items() if presents >= count)
 
 
 if __name__ == '__main__':
@@ -23,8 +37,8 @@ if __name__ == '__main__':
     expected_1 = [(8, [150])]  # [(<answer>, [<input>, *part1_args])]
     func_1 = solve
 
-    part2_args = []
-    expected_2 = []  # [<answer>, [(<input>, *part2_args)]]
+    part2_args = [11, 50]
+    expected_2 = [(8, [150])]  # [<answer>, [(<input>, *part2_args)]]
     func_2 = solve
 
     if expected_1:
