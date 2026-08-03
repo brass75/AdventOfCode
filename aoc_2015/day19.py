0d2dd41a2045f02bee6def4413db194810bf91c9
Dan Shernicoff <dshernicoff@tenable.com>
2026-08-03 14:13:15 -0400

    Day 19 part 2

--- aoc_2015/day19.py
+++ aoc_2015/day19.py
@@ -1,21 +1,21 @@
-import heapq
+import re
 from collections.abc import Iterator
 from pathlib import Path
 
 from aoc_lib import solve_problem
 
 
-def parse_input(input_: str) -> tuple[tuple[tuple[str, str], ...], str]:
+def parse_input(input_: str) -> tuple[dict[str, str], str]:
     params, start = input_.strip().split('\n\n')
-    replacements: list[tuple[str, str]] = []
+    replacements: dict[str, str] = {}
     for line in params.splitlines():
         if not line.strip():
             continue
         line = line.strip()
         atom, replacement = line.split(' => ')
-        replacements.append((atom, replacement))
+        replacements[replacement[::-1]] = atom[::-1]
 
-    return tuple(replacements), start
+    return replacements, start
 
 
 INPUT = parse_input(Path('data/day19.txt').read_text())
@@ -39,48 +39,32 @@ O => HH
 HOHOHO""")
 
 
-entry_cache = {}
-
-
-def do_replacement(start: str, replacements: tuple[tuple[str, str], ...]) -> Iterator[str]:
-    global entry_cache
-    if cached := entry_cache.get((start, replacements)):
-        yield from cached
-        return
-
-    entry_cache[(start, replacements)] = set()
+def do_replacement(start: str, replacements: dict[str, str]) -> Iterator[tuple[str, int]]:
     for i in range(len(start)):
-        for atom, replacement in replacements:
+        for replacement, atom in replacements.items():
             if start[i:].startswith(atom):
-                possibility = f'{start[:i]}{replacement}{start[i + len(atom) :]}'
-                if possibility in entry_cache[(start, replacements)] or entry_cache[(start, replacements)].add(
-                    possibility
-                ):
-                    continue
-                yield possibility
+                yield f'{start[:i]}{replacement}{start[i + len(atom) :]}', len(atom)
 
 
-def solve(input_: tuple[tuple[tuple[str, str], ...], str]) -> int:
+def solve(input_: tuple[dict[str, str], str]) -> int:
     replacements, start = input_
-    possibilities: set[str] = set()
-    possibilities.update(do_replacement(start, replacements))
-    return len(possibilities)
+    return len({possibility for possibility, _ in do_replacement(start[::-1], replacements)})
 
 
-def solve2(input_: tuple[tuple[tuple[str, str], ...], str]) -> int:
+def solve2(input_: tuple[dict[str, str], str]) -> int:
     replacements, end = input_
-    start = 'e'
-    q = [(0, start)]
-    while q:
-        count, curr = heapq.heappop(q)
-        for next_molecule in do_replacement(curr, replacements):
-            # print(f'{count = } {curr = } {next_molecule = } {end = }')
-            if next_molecule == end:
-                return count + 1
-            if count and not end.startswith(next_molecule[:count]):
-                continue
-            q.append((count + 1, next_molecule))
-    return -1
+    end = end[::-1]
+
+    pattern = r'|'.join(replacements.keys())
+
+    def replace(s: re.Match):
+        return replacements[s.group()]
+
+    count = 0
+    while end != 'e':
+        count += 1
+        end = re.sub(pattern, replace, end, count=1)
+    return count
 
 
 if __name__ == '__main__':
@@ -89,7 +73,7 @@ if __name__ == '__main__':
     func_1 = solve
 
     part2_args = []
-    expected_2 = [(3, [TEST_INPUT]), (6, [TEST_INPUT2])]  # [<answer>, [(<input>, *part2_args)]]
+    expected_2 = [(0, [({}, 'e')])]  # The algorithm doesn't work for the test input.
     func_2 = solve2
 
     if expected_1:
